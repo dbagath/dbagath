9e96c502e5005d7c5ee736dbf35c4b61d1b972a0
dbagath <>
2021-06-04 04:40:40 +0000

    Updated files

--- Python3/mapandreduce.py
+++ Python3/mapandreduce.py
@@ -11,3 +11,9 @@ lst = [10, 5, 5, 5]
 lst = reduce(lambda x, y: x + y, lst)
 
 print(lst)
+
+scores = [66, 90, 68, 59, 76, 60, 88, 74, 81, 65]
+
+over_75 = list(filter(lambda x: 80 > x > 60, scores))
+
+print(over_75)
